f295bcbc39e91c22ae66127f7107a282ecf8ae57
waleedhesham446 <waleedhesham2001@gmail.com>
2024-07-01 01:46:31 +0300

    Section 1 | Spring-Boot overview

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return coachName + " is the best!";
+    }
+}
---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,27 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return coachName + " is the best!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+}
