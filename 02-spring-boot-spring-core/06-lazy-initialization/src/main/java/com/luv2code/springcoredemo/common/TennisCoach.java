115133c1450dedb9c645dc69175d6b5325d53d6d
waleedhesham446 <waleedhesham2001@gmail.com>
2024-07-21 20:36:04 +0300

    Section 2 | P6 | Lazy Initialization

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+  public TennisCoach() {
+    System.out.println("In constructor " + this.getClass().getName());
+  }
+
+  @Override
+  public String getDailyWorkout() {
+    return "Practice your backhand volley";
+  }
+}
