27deed399f9d8704cd2671911a367459c70f0a67
waleedhesham446 <waleedhesham2001@gmail.com>
2024-07-21 18:36:11 +0300

    Section 2 | P3 | Setter Injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,30 @@
+package com.luv2code.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springcoredemo.common.Coach;
+
+@RestController
+public class DemoController {
+
+    /*
+     * Field injection
+        @Autowired
+        private Coach myCoach;
+    */
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
